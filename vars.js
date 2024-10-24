7fe09ba489aeeb51b498c284481c529ca5f306d7
TsvetkovM <149527029+TsvetkovM@users.noreply.github.com>
2024-10-24 21:20:51 +0300

    first commit

---
+++ vars.js
@@ -0,0 +1,43 @@
+// ---------1----------
+const age = 15;
+console.log(age);
+// ---------2--------
+const name = ("Matvii");
+console.log(name);
+// -----------3-------
+const isStudent = true;
+// ----------4----------
+const myString = ("завжди мийте тарілку після гречки");
+console.log(myString);
+// ----------5---------
+const myNumber = 239;
+console.log(myNumber+10);
+// -------------6-----------
+const myNull = null;
+// ---------------7-----------
+const userName = prompt("Enter name");
+const greeting = "Hello " + userName;
+console.log(greeting);
+// -----------8----------
+const keepSub = confirm("Do you want to continue");
+console.log(keepSub);
+if (keepSub ){
+    alert("Дякую за підтвердження!");
+ } else {
+     alert("Дію відмінено!");
+  }
+// -------------9-------------
+const myAlert = confirm("Небезпечна дія!");
+console.log(myAlert);
+if (myAlert ){
+    alert("Дякую за підтвердження!");
+ } else {
+     alert("Дію відмінено!");
+  }
+
+
+
+
+
+
+
